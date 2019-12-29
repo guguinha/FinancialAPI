700acb8fc52d2d02df8e81f8c873c6e407f7d0ea
Augusto Guimarães <guguinha7@gmail.com>
2019-12-29 19:20:19 -0300

    Add banco de teste H2

--- src/main/java/br/com/guimaraes/augusto/financial/api/resources/FinancaResource.java
+++ src/main/java/br/com/guimaraes/augusto/financial/api/resources/FinancaResource.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value="/Financa")
+@RequestMapping(value="/financa")
 public class FinancaResource {
 	
 	@GetMapping(value="/{id}")
