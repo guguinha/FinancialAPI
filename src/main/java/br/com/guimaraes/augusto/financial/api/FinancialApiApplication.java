190c4abbf295cfdc8b2f6e357cab1a2bb804679d
Augusto Guimarães <guguinha7@gmail.com>
2019-12-29 17:36:52 -0300

    Projeto criado

---
+++ src/main/java/br/com/guimaraes/augusto/financial/api/FinancialApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.guimaraes.augusto.financial.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/guimaraes/augusto/financial/api/FinancialApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.guimaraes.augusto.financial.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
